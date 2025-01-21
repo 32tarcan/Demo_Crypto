e80e03ff5e224a3d9f63d12db652a062796e2c01
Bahadır Tarcan <tarcann.bahadir@gmail.com>
2025-01-22 00:49:02 +0300

    Initial Commit

---
+++ Demo_Crypto/Demo_CryptoApp.swift
@@ -0,0 +1,17 @@
+//
+//  Demo_CryptoApp.swift
+//  Demo_Crypto
+//
+//  Created by Bahadır Tarcan on 22.01.2025.
+//
+
+import SwiftUI
+
+@main
+struct Demo_CryptoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
